bde18388d71d8af93b7b577791b62b9128078abf
Nathan Marz <nathan@nathanmarz.com>
2015-05-05 09:32:34 -0400

    notes

--- src/clj/com/rpl/specter/protocols.clj
+++ src/clj/com/rpl/specter/protocols.clj
@@ -1,6 +1,10 @@
 (ns com.rpl.specter.protocols)
 
 
+;;TODO: might be able to speed it up more by having these return a function rather than 
+;;rely on protocol methods (can then compose functions together directly)
+;;so protocol would just be used during composition - what about execution?
+;;could have a select-fast function that takes in a selector FUNCTION explicitly
 (defprotocol StructureValsPath
   (select-full* [this vals structure next-fn])
   (update-full* [this vals structure next-fn]))
