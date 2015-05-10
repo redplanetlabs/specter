e052ee06a8df85778e587537e9610e961085b59c
Nathan Marz <nathan@nathanmarz.com>
2015-05-10 18:39:09 -0400

    removed plugin

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"] ; this prevents JVM from doing optimizations which can remove stack traces from NPE and other exceptions
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
-  :plugins [[lein-nodisassemble "0.1.3"]]
   :profiles {:dev {:dependencies
                    [[org.clojure/test.check "0.5.9"]]}
              })
