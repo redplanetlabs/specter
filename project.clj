86a3aee11d1f9929ffda1452759a78c702cf4792
Nathan Marz <nathan@nathanmarz.com>
2015-05-31 08:50:21 -0400

    0.5.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rpl/specter "0.5.0"
+(defproject com.rpl/specter "0.5.1"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"] ; this prevents JVM from doing optimizations which can remove stack traces from NPE and other exceptions
