2404445827a820442bec68137570edf869009119
Pietro F. Menna <pietromenna@yahoo.com>
2015-06-24 18:37:58 -0400

    Added cljsbuild support

--- project.clj
+++ project.clj
@@ -4,8 +4,16 @@
   :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  ]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"] ; this prevents JVM from doing optimizations which can remove stack traces from NPE and other exceptions
+  :plugins [[lein-cljsbuild "1.0.6"]]
+  :hooks [leiningen.cljsbuild]
   :source-paths ["src"]
   :test-paths ["test/clj"]
   :profiles {:dev {:dependencies
                    [[org.clojure/test.check "0.5.9"]]}
-             })
+             }
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src"]
+        :compiler {
+          :output-to "target/cljsbuild-main.js"}}]
+    })
