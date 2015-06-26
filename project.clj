64168263743ba6a4cff0706e98dab3e5f5ae4910
Pietro F. Menna <pietromenna@yahoo.com>
2015-06-26 13:35:18 -0400

    Changed directory structure for test directory to be the same as for src

--- project.clj
+++ project.clj
@@ -6,13 +6,14 @@
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"] ; this prevents JVM from doing optimizations which can remove stack traces from NPE and other exceptions
   :plugins [[lein-cljsbuild "1.0.6"]]
   :source-paths ["src"]
-  :test-paths ["test/clj"]
+  :test-paths ["test"]
   :profiles {:dev {:dependencies
                    [[org.clojure/test.check "0.5.9"]]}
              }
   :cljsbuild {
     :builds [{
         :source-paths ["src"]
+        :test-paths ["test"]
         :compiler {
           :output-to "target/cljsbuild-main.js"}}]
     })
