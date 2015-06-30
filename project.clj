784fb9fb8dc3ea0ccc3487ef47eec0a76b3f724b
Nathan Marz <nathan@nathanmarz.com>
2015-06-30 19:53:17 -0400

    add clojurescript dep and update clojure dep to 1.7.0, fix cljsbuild definition

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (def VERSION (.trim (slurp "VERSION")))
 
 (defproject com.rpl/specter VERSION
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 ;;TODO: how to make this a dep of only the cljs version?
+                 [org.clojure/clojurescript "0.0-3308"]
                  ]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"] ; this prevents JVM from doing optimizations which can remove stack traces from NPE and other exceptions
   :plugins [[lein-cljsbuild "1.0.6"]]
@@ -11,9 +13,12 @@
                    [[org.clojure/test.check "0.7.0"]]}
              }
   :cljsbuild {
-    :builds [{
-        :source-paths ["src"]
-        :test-paths ["test"]
-        :compiler {
-          :output-to "target/cljsbuild-main.js"}}]
+    :builds {:dev
+             {:source-paths ["src"]
+              :compiler {
+                         :output-to "target/main.js"
+                         :optimizations :whitespace
+                         :pretty-print true
+                         }}
+             }
     })
