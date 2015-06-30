0a0bab144b032b1d030c849456ccae1b5bf78549
Nathan Marz <nathan@nathanmarz.com>
2015-06-30 18:09:03 -0400

    add cljs test helpers file

---
+++ test/com/rpl/specter/cljs_test_helpers.clj
@@ -0,0 +1,16 @@
+(ns com.rpl.specter.cljs-test-helpers
+  (:require [clojure.test.check             
+             [generators :as gen]
+             [properties :as prop]]))
+
+;; it seems like gen/bind and gen/return are a monad (hence the names)
+(defmacro for-all+ [bindings & body]
+  (let [parts (partition 2 bindings)
+        vars (vec (map first parts))
+        genned (reduce
+                (fn [curr [v code]]
+                  `(cljs.test.check.generators/bind ~code (fn [~v] ~curr)))
+                `(cljs.test.check.generators/return ~vars)
+                (reverse parts))]
+    `(cljs.test.check.properties/for-all [~vars ~genned]
+                   ~@body )))
