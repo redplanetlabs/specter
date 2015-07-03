1d93961cd3d052a6b632f6adb58b31f56bbd9b4d
Nathan Marz <nathan@nathanmarz.com>
2015-07-03 17:40:13 -0400

    fix not-native tag for optimized cljs prot invocation

--- src/com/rpl/specter/prot_opt_invoke.clj
+++ src/com/rpl/specter/prot_opt_invoke.clj
@@ -1,10 +1,11 @@
 (ns com.rpl.specter.prot-opt-invoke)
 
 (defmacro mk-optimized-invocation [protocol obj method num-args]
-  (let [args (take num-args (repeatedly gensym))]
+  (let [args (take num-args (repeatedly gensym))
+  	    o (-> (gensym) (with-meta {:tag 'not-native}))]
     `(if (~'implements? ~protocol ~obj)
-       (fn [^not-native o# ~@args]
-         (~method o# ~@args)
+       (fn [~o ~@args]
+         (~method ~o ~@args)
          )
        ~method
        )))
