6b3ea3279915567dc76caba2dfb3696f233da0f9
Nathan Marz <nathan@nathanmarz.com>
2016-10-14 09:52:55 -0400

    update versions of clj and cljs for tests, new repl script for cljs

--- DEVELOPER.md
+++ DEVELOPER.md
@@ -7,7 +7,7 @@ lein cleantest
 # Running ClojureScript tests
 
 ```
-rm -rf out/
+rm -rf .cljs_node_repl
 lein javac
 rlwrap java -cp `lein classpath` clojure.main repl.clj
 (require 'com.rpl.specter.cljs-test-runner)
--- repl.clj
+++ repl.clj
@@ -1,20 +1,5 @@
-(require 'cljs.repl)
-(require 'cljs.build.api)
-(require 'cljs.repl.node)
+(require
+  '[cljs.repl :as repl]
+  '[cljs.repl.node :as node])
 
-(cljs.build.api/build "target/classes/com/rpl"
-  {:output-to "out/main.js"
-   :verbose true
-   :warning-handlers [(fn [warning-type env extra]
-                       (when (warning-type cljs.analyzer/*cljs-warnings*)
-                         (when-let [s (cljs.analyzer/error-message warning-type extra)]
-                           (binding [*out* *err*]
-                             (println "WARNING:" (cljs.analyzer/message env s))
-                             (println "Failed to build because of warning!"))
-
-                           (System/exit 1))))]})
-
-(cljs.repl/repl (cljs.repl.node/repl-env)
-  :watch "target/classes/com/rpl"
-  :output-dir "out"
-  :static-fns true)
+(repl/repl (node/repl-env))
