4695c9699879c2dd6def787dd167f8c3a370e9f2
Mayank Jain <mayank@helpshift.com>
2017-03-11 01:24:45 +0530

    Add cljs tests to travis
    * Add lein-doo plugin to run cljs tests. This is the new recommended
      approach.
    * Update project.clj to add cljsbuild config
    * Update .travis.yml file to run cljs tests as well.
    * Also to speed up travis builds add ~/.m2 dir in cache
    
    To run do:
    $ lein do javac, doo phantom test-build once
    
    Note that you'll need phantomjs installed for above to work.

--- test/com/rpl/specter/cljs_test_runner.cljs
+++ test/com/rpl/specter/cljs_test_runner.cljs
@@ -1,8 +1,7 @@
 (ns com.rpl.specter.cljs-test-runner
-  (:require [cljs.test :as test :refer-macros [run-tests]]
+  (:require [doo.runner :refer-macros [doo-tests]]
             [com.rpl.specter.core-test]
             [com.rpl.specter.zipper-test]))
 
-
-(run-tests 'com.rpl.specter.core-test)
-(run-tests 'com.rpl.specter.zipper-test)
+(doo-tests 'com.rpl.specter.core-test
+           'com.rpl.specter.zipper-test)
